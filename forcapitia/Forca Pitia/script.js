a04294980fbbe300e62cfa36b9c027d235dd45b5
pitialaab <pitialab.023@gmail.com>
2023-05-28 23:06:29 -0300

    ....

--- forcapitia/Forca Pitia/script.js
+++ forcapitia/Forca Pitia/script.js
@@ -1,4 +1,3 @@
-
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
 const userInputSection = document.getElementById("user-input-section");
@@ -6,7 +5,6 @@ const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
-
 
 let options = {
   iniciar: [
@@ -19,15 +17,13 @@ let options = {
   ]
 };
 
-
 let winCount = 0;
 let count = 0;
 
 let chosenWord = "";
 
-
 const displayOptions = () => {
-  optionsContainer.innerHTML += `<h3>Forca Pititinha</h3>`;
+  optionsContainer.innerHTML += `<h3>Forca Pititica</h3>`;
   let buttonCon = document.createElement("div");
   for (let value in options) {
     buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
@@ -35,26 +31,23 @@ const displayOptions = () => {
   optionsContainer.appendChild(buttonCon);
 };
 
-
 const blocker = () => {
   let optionsButtons = document.querySelectorAll(".options");
   let letterButtons = document.querySelectorAll(".letters");
-  
+
   optionsButtons.forEach((button) => {
     button.disabled = true;
   });
 
-  
   letterButtons.forEach((button) => {
-    button.disabled.true;
+    button.disabled = true;
   });
   newGameContainer.classList.remove("hide");
 };
 
-
 const generateWord = (optionValue) => {
   let optionsButtons = document.querySelectorAll(".options");
-  
+
   optionsButtons.forEach((button) => {
     if (button.innerText.toLowerCase() === optionValue) {
       button.classList.add("active");
@@ -62,85 +55,73 @@ const generateWord = (optionValue) => {
     button.disabled = true;
   });
 
-  
   letterContainer.classList.remove("hide");
   userInputSection.innerText = "";
 
   let optionArray = options[optionValue];
-  
+
   chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
   chosenWord = chosenWord.toUpperCase();
 
-  
   let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
 
-  
   userInputSection.innerHTML = displayItem;
 };
-
 
 const initializer = () => {
   winCount = 0;
   count = 0;
 
- 
   userInputSection.innerHTML = "";
   optionsContainer.innerHTML = "";
   letterContainer.classList.add("hide");
   newGameContainer.classList.add("hide");
   letterContainer.innerHTML = "";
 
-  
   for (let i = 65; i < 91; i++) {
     let button = document.createElement("button");
     button.classList.add("letters");
-    
+
     button.innerText = String.fromCharCode(i);
-   
+
     button.addEventListener("click", () => {
       let charArray = chosenWord.split("");
       let dashes = document.getElementsByClassName("dashes");
-      
+
       if (charArray.includes(button.innerText)) {
         charArray.forEach((char, index) => {
-          
+
           if (char === button.innerText) {
-            
             dashes[index].innerText = char;
-           
             winCount += 1;
-            
+
             if (winCount == charArray.length) {
-              resultText.innerHTML = `<h2 class='win-msg'>Voce ganhou!!</h2><p>A palavra era <span>${chosenWord}</span></p>`;
-              
+              resultText.innerHTML = `<h2 class='win-msg'>Você ganhou!!</h2><p>A palavra era <span>${chosenWord}</span></p>`;
               blocker();
             }
           }
         });
       } else {
-        
         count += 1;
-        
         drawMan(count);
-        
+
         if (count == 6) {
-          resultText.innerHTML = `<h2 class='lose-msg'>Voce perdeu!!</h2><p>A palavra era <span>${chosenWord}</span></p>`;
+          resultText.innerHTML = `<h2 class='lose-msg'>Você perdeu!!</h2><p>A palavra era <span>${chosenWord}</span></p>`;
           blocker();
         }
       }
-      
+
       button.disabled = true;
     });
     letterContainer.append(button);
   }
 
   displayOptions();
-  
+
   let { initialDrawing } = canvasCreator();
-  
+
   initialDrawing();
 };
-
 
 const canvasCreator = () => {
   let context = canvas.getContext("2d");
@@ -148,7 +129,6 @@ const canvasCreator = () => {
   context.strokeStyle = "#000";
   context.lineWidth = 2;
 
-  
   const drawLine = (fromX, fromY, toX, toY) => {
     context.moveTo(fromX, fromY);
     context.lineTo(toX, toY);
@@ -181,23 +161,16 @@ const canvasCreator = () => {
     drawLine(70, 80, 90, 110);
   };
 
- 
   const initialDrawing = () => {
-   
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    
     drawLine(10, 130, 130, 130);
-   
     drawLine(10, 10, 10, 131);
-    
     drawLine(10, 10, 70, 10);
-    
     drawLine(70, 10, 70, 20);
   };
 
   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
-
 
 const drawMan = (count) => {
   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
@@ -225,10 +198,8 @@ const drawMan = (count) => {
   }
 };
 
-
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
-
 
 // Obtenha uma referência para o elemento de áudio
 const backgroundMusic = document.getElementById('background-music');
@@ -242,7 +213,3 @@ function pausarMusica() {
 function retomarMusica() {
   backgroundMusic.play();
 }
-
-
-
-
